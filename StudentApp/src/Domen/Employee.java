4bbcdac41245558c172b199415949b68e2593256
Golikov-Andrey <newsc2@yandex.ru>
2023-09-01 22:14:26 +0300

    solution from sem 3 OOP StudentApp

---
+++ StudentApp/src/Domen/Employee.java
@@ -0,0 +1,9 @@
+package Domen;
+
+public class Employee extends Person{
+    private String special;
+    public Employee(String firstName, int age, String special) {
+        super(firstName, age);
+        this.special = special;
+    }
+}
